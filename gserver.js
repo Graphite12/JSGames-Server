9a3c3ab671e081f52968298b0db416fd9a1ddcff
hsh411 <pixeldot12@gmail.com>
2021-06-27 21:30:30 +0900

    Add Sequelize, cors, and Update gserver.js
    Completed 50% implementation of backend server foundation to implement login system

--- gserver.js
+++ gserver.js
@@ -2,12 +2,24 @@ const express = require("express");
 const https = require("https");
 const path = require("path");
 const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3333;
+
+const corsOption = {
+  origin: true,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTION"],
+};
+
+app.use(cors(corsOption));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-  res.send("Hello!!! SSL Server");
+  res.json({ message: "Hello!!! SSL Server" });
 });
 
 const sslServer = https.createServer(
